48a352a2d42cd7522a4d187e22428cb7682c9838
goodie007 <aruoturegoodness@gmail.com>
2021-12-16 05:07:10 +0100

    smartchat

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import React, { useState } from 'react';
+import React,{ useState }  from 'react';
 import './App.css';
 import Register from './components/Register';
 import Login from './components/Login';
+import useToken from './components/useToken';
+
+
 
 function App() {
-  const [token, setToken] = useState();
-
-  if(!token) {
-    return <Login setToken={setToken} />
-  }
-
+  
 
   return (
     <div className="App">
-      <Login />
+      <Register />
     </div>
   );
 }
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,8 +1,43 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
 
-export default class Register extends Component {
-    render() {
+
+
+
+
+export default function Register() {
+    const [username, setUsername] = useState();
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        const summit = await registerUser({
+          username,
+          email,
+          password
+        });
+    }
+
+    async function registerUser() {
+        const items = {username, email, password}
+
+        const register = await fetch('', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify()
+        })
+        register = await register.json()
+        localStorage.setItem("user-info", JSON.stringify(register))
+        
+
+
+         .then(data => data.json())
+    }
+
         return (
             <form className="forms">
                 <h3>Sign Up</h3>
@@ -11,28 +46,27 @@ export default class Register extends Component {
                     <div className="label">
                         <label>Username</label>
                     </div>
-                    <input type="text" className="form-control" placeholder="Username" />
+                    <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="form-control" placeholder="Username" />
                 </div>
                 
                 <div className="form-group">
                     <div className="label">
                         <label>Email address</label>
                     </div>
-                    <input type="email" className="form-control" placeholder="Enter email" />
+                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} className="form-control" placeholder="Enter email" />
                 </div>
 
                 <div className="form-group">
                     <div className="label">
                         <label>Password</label>
                     </div>
-                    <input type="password" className="form-control" placeholder="Enter password" />
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="form-control" placeholder="Enter password" />
                 </div>
 
-                <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
+                <button type="submit" className="btn btn-primary btn-block" onClick={handleSubmit}>Sign Up</button>
                 <p className="forgot-password text-right">
                     Already registered <a href="Login">sign in?</a>
                 </p>
         </form>
-        );
-    }
-}
+    );
+};
