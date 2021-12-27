bf3b53722796e60fdb2fb16392b34c065eee1ec5
goodie007 <aruoturegoodness@gmail.com>
2021-12-27 10:09:47 +0100

    smartchat

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -7,65 +7,24 @@ import Model3 from '../images/Model3.jpeg';
 
 export default function ContactCard({ contactList }){
     return(
-        <div className='inputField'>
-            <figure className='fig'>
-               <img src={Model3} alt="user" className='images'/> 
+        
+        {contactList?.map((contact, index) => (
+            <div className='inputField'>
+            <figure className='fig' key={index}>
+               <img src={contact.picture.large} alt="user" className='images'/> 
                <figcaption>
-                   <p>Username</p>
-                   <p>email address</p>
-               </figcaption>
-            </figure>
-            <figure className='fig'>
-               <img src={Model3} alt="user" className='images'/> 
-               <figcaption>
-                   <p>Username</p>
-                   <p>email address</p>
-               </figcaption>
-            </figure>
-            <figure className='fig'>
-               <img src={Model3} alt="user" className='images'/> 
-               <figcaption>
-                   <p>Username</p>
-                   <p>email address</p>
-               </figcaption>
-            </figure>
-            <figure className='fig'>
-               <img src={Model3} alt="user" className='images'/> 
-               <figcaption>
-                   <p>Username</p>
-                   <p>email address</p>
-               </figcaption>
-            </figure>
-            <figure className='fig'>
-               <img src={Model3} alt="user" className='images'/> 
-               <figcaption>
-                   <p>Username</p>
-                   <p>email address</p>
-               </figcaption>
-            </figure>
-            <figure className='fig'>
-               <img src={Model3} alt="user" className='images'/> 
-               <figcaption>
-                   <p>Username</p>
-                   <p>email address</p>
-               </figcaption>
-            </figure>
-            <figure className='fig'>
-               <img src={Model3} alt="user" className='images'/> 
-               <figcaption>
-                   <p>Username</p>
-                   <p>email address</p>
-               </figcaption>
-            </figure>
-            <figure className='fig'>
-               <img src={Model3} alt="user" className='images'/> 
-               <figcaption>
-                   <p>Username</p>
-                   <p>email address</p>
+                   <p>
+                       <span>username: </span>{contact.username}
+                   </p>
+                   <p>
+                       <span> email: </span>{contact.email}
+                   </p>
                </figcaption>
             </figure>
             
+            
         </div>
+        ))}
 
     )
 
